7a5053e1077b9b57b991fdd933fed97424059d4f
SaiDhinakar <saidhinakars899@gmail.com>
2025-03-26 19:30:02 +0530

    two sum

--- Two Sum .py
+++ Two Sum .py
@@ -2,9 +2,9 @@ def TwoSum(nums, target):
     seen = {}
     for idx, num in enumerate(nums):
         complement = target-num
-        if complent in seen:
+        if complement in seen:
             return [seen[complement], idx]
-        seen[num] = i
+        seen[num] = idx
     return [-1,-1]
 
 # inputs
